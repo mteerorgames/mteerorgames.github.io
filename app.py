1eee02b412c3f340bd797db00e0680078b45a571
mteerorgames <mteerorgames@gmail.com>
2026-01-21 10:37:22 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, send_from_directory, jsonify
+import os
+
+app = Flask(__name__)
+CARPETA = 'archivos'
+os.makedirs(CARPETA, exist_ok=True)
+
+@app.route('/')
+def inicio():
+    return open('index.html').read()
+
+@app.route('/subir', methods=['POST'])
+def subir():
+    archivo = request.files['archivo']
+    archivo.save(os.path.join(CARPETA, archivo.filename))
+    return 'Archivo subido. <a href="/">Volver</a>'
+
+@app.route('/archivos/<nombre>')
+def descargar(nombre):
+    return send_from_directory(CARPETA, nombre, as_attachment=True)
+
+@app.route('/archivos')
+def listar():
+    return jsonify(os.listdir(CARPETA))
+
+if __name__ == '__main__':
+    app.run(debug=True)
